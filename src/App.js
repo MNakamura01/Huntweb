229ff65215619698a1ecc6456263d54a59670ad0
mnakamura01 <mrnnakamura01@gmail.com>
2020-06-27 01:54:04 -0300

    Principais interfaces completas

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from './components/Header'
 import api from './services/api'
 import Main from './pages/main'
 
+import Routes from './routes'
+
 import './styles.css'
 
 const App = () => (
     <div className="App">
       <Header />
-      <Main />
+      <Routes />
     </div>
 )
 
